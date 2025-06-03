b343ee376d220f2cf51c8832b29609fcc07128b8
Shamir D <shamx9ir@gmail.com>
2025-06-03 18:04:30 +0100

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-base-java
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_base_java/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-base-java.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-base-java.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
